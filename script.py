bba305ec0c7ef90cd4beb2657b54c43a90d99e90
Lakshmansai1999 <94889893+Lakshmansai1999@users.noreply.github.com>
2023-10-12 15:36:43 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,13 +1,5 @@
-import sys
 import os
 
-param_name = os.getenv('param_name')
-param_name = sys.argv[0] 
-#print("param_name is " + param_name)
+param_value = os.getenv('MY_PARAM')
 
-# Process the parameter value
-processed_data = param_name.upper()  # Example: Convert to uppercase
-
-# Store the data (e.g., in a file)
-with open('output.txt', 'w') as f:
-    f.write(processed_data)
+print(f"The parameter value is: {param_value}")
